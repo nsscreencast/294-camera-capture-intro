ff4943c86dc9afe0f814a7dad53a2c5e15bed703
Ben Scheirman <ben@scheirman.com>
2017-08-02 14:06:42 -0500

    Episode 294

---
+++ 1. RectCapture-starting/RectCapture/CaptureViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  RectCapture
+//
+//  Created by Ben Scheirman on 6/27/17.
+//  Copyright © 2017 NSScreencast. All rights reserved.
+//
+
+import UIKit
+
+class CaptureViewController: UIViewController {
+    
+}
+
